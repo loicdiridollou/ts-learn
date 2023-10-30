5fb1ba00a32c4d01281bff166467200c52059f45
Loic Diridollou <loic.diridollou@gmail.com>
2023-10-30 16:39:37 -0700

    Making progress

--- dist/module03.js
+++ dist/module03.js
@@ -5,3 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const creds_json_1 = __importDefault(require("./creds.json"));
 console.log(creds_json_1.default);
+let x;
+x = 40;
+console.log(x + 1);
+let flag;
+let yes = true;
+let no = false;
+console.log(yes, no);
--- src/module03.ts
+++ src/module03.ts
@@ -1,3 +1,13 @@
 import credsJson from "./creds.json";
 
 console.log(credsJson);
+
+let x: number;
+x = 40;
+console.log(x + 1);
+
+let flag: boolean;
+let yes = true;
+let no = false;
+
+console.log(yes, no);
