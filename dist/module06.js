9c9cfdb981ca6a56aec9370a2ca4ae91a3d5d1fc
Loic Diridollou <loic.diridollou@gmail.com>
2023-11-11 17:38:49 -0800

    Adding details about module 6

---
+++ dist/module06.js
@@ -0,0 +1,17 @@
+"use strict";
+// module 06 - union and intersection of types
+let test_var;
+test_var = 8;
+test_var = false;
+function add(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x + y;
+    }
+    if (typeof x === "string" && typeof y === "string") {
+        return x.concat(y);
+    }
+    throw new Error("Parameters must be numbers or strings");
+}
+console.log(add("one", "two")); //* Returns "onetwo"
+console.log(add(1, 2)); //* Returns 3
+console.log(add("one", 2)); //* Returns error
