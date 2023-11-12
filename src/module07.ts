50b0e82b9b8ff27d6363ef12254f2539c972cbea
Loic Diridollou <loic.diridollou@gmail.com>
2023-11-11 17:39:33 -0800

    Adding module 7 about collection types

---
+++ src/module07.ts
@@ -0,0 +1,8 @@
+// collection types in TypeScript
+
+let list: number[] = [1, 2, 3];
+let list1: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ["Marcia", 35];
+
+console.log(person1);
