ccc18aeaec8cfde02d10e897d4d187f00bb95aeb
Loic Diridollou <loic.diridollou@gmail.com>
2023-11-14 20:31:50 -0800

    Adding unit 7 for module 5 about static public elements of a class

--- dist/module05/unit07.js
+++ dist/module05/unit07.js
@@ -1,12 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NewCar = void 0;
 // static properties of class
-class Car {
+class NewCar {
     // Constructor
     constructor(make, color, doors = 4) {
         this._make = make;
         this._color = color;
         this._doors = doors;
-        Car.numberOfCars++; // Increments the value of the static property
+        NewCar.numberOfCars++; // Increments the value of the static property
     }
     // accessors
     get make() {
@@ -22,8 +24,9 @@ class Car {
         return this.numberOfCars;
     }
 }
+exports.NewCar = NewCar;
 // Properties
-Car.numberOfCars = 0; // New static property
-let car1 = new Car("make1", "color1", 2);
-let car2 = new Car("make2", "color2", 4);
-console.log(Car.getNumberOfCars());
+NewCar.numberOfCars = 0; // New static property
+let car1 = new NewCar("make1", "color1", 2);
+let car2 = new NewCar("make2", "color2", 4);
+//console.log(NewCar.getNumberOfCars());
--- src/module05/unit07.ts
+++ src/module05/unit07.ts
@@ -1,5 +1,5 @@
 // static properties of class
-class Car {
+export class NewCar {
   // Properties
   private static numberOfCars: number = 0; // New static property
   private _make: string;
@@ -11,7 +11,7 @@ class Car {
     this._make = make;
     this._color = color;
     this._doors = doors;
-    Car.numberOfCars++; // Increments the value of the static property
+    NewCar.numberOfCars++; // Increments the value of the static property
   }
 
   // accessors
@@ -30,7 +30,7 @@ class Car {
   }
 }
 
-let car1 = new Car("make1", "color1", 2);
-let car2 = new Car("make2", "color2", 4);
+let car1 = new NewCar("make1", "color1", 2);
+let car2 = new NewCar("make2", "color2", 4);
 
-console.log(Car.getNumberOfCars());
+//console.log(NewCar.getNumberOfCars());
