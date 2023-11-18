571968a7a53a64889f86525973db90e119ee3fbd
Loic Diridollou <loic.diridollou@gmail.com>
2023-11-17 18:36:16 -0800

    Adding elements about extending classes

---
+++ src/module05/unit09.ts
@@ -0,0 +1,38 @@
+import { NewCar } from "./unit07";
+
+// extending a class with inheritance
+
+class ElectricCar extends NewCar {
+  // Properties
+  private _range: number; // Properties unique to ElectricCar
+
+  // Constructor
+  constructor(make: string, color: string, range: number, doors = 2) {
+    super(make, color, doors);
+    this._range = range;
+  }
+
+  // Accessors
+  get range() {
+    return this._range;
+  }
+
+  // Methods
+  charge() {
+    console.log(this.worker() + " is charging.");
+  }
+  // Overrides the brake method of the Car class
+  brake(): string {
+    return `${this.worker()} is braking with the regenerative braking system.`;
+  }
+  // This function performs work for the other method functions
+  worker(): string {
+    return this.make;
+  }
+}
+
+let spark = new ElectricCar("Spark Motors", "silver", 124, 2);
+let eCar = new ElectricCar("Electric Car Co.", "black", 263);
+console.log(eCar.doors); // returns the default, 2
+spark.charge(); // returns "Spark Motors is charging"
+console.log(spark.brake()); // returns "Spark Motors is braking with the regenerative braking system"
